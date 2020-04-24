45e35a00727244b33d25e124b9333f18d4449b6a
Hrishikesh-3459 <hrishikeshmm01@gmail.com>
2020-04-24 14:36:20 +0530

    Added solution to problem 20

---
+++ prob_20.py
@@ -0,0 +1,17 @@
+def sockMerchant(n, ar):
+    colours = []
+    pair = 0
+    for i in range(n):
+        if(ar[i] not in colours):
+            colours.append(ar[i])
+        else:
+            pair += 1
+            colours.remove(ar[i])
+    return(pair)
+
+
+print(sockMerchant(9, [10,20,20,10,10,30,50,10,20]))
+
+# x = [10,20,20,10,10,30,50,10,20]
+# x.pop()
+# print(x)
