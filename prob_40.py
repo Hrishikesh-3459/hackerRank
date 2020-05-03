24b720e8e9fcb3495c64fe21f7a7d791ad58d717
Hrishikesh-3459 <hrishikeshmm01@gmail.com>
2020-05-03 09:01:59 +0530

    Added solution to problem 40

--- prob_40.py
+++ prob_40.py
@@ -1,6 +1,11 @@
 def libraryFine(d1, m1, y1, d2, m2, y2):
-    if (d2 >= d1 and m2 >= m1 and y2 >= y1):
+    if (y1 < y2):
         return(0)
+    if (m1 < m2 and y1 <= y2):
+        return(0)
+    if (d1 <= d2 and y1 <= y2 and m1 <= m2):
+        return(0)
+    
     if (y1 != y2):
         return("10000")
     if (m1 != m2):
