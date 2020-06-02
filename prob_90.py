0da7fdf648f66e20ad24de5bc724d19e4bfcd34e
Hrishikesh Mulkutkar <51927760+Hrishikesh-3459@users.noreply.github.com>
2020-06-02 13:15:08 +0530

    Fibonacci Modified

---
+++ prob_90.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the fibonacciModified function below.
+def fibonacciModified(t1, t2, n):
+    fib = [t1, t2]
+    while len(fib) != n:
+        val = t1 + (t2 ** 2)
+        fib.append(val)
+        t1 = t2
+        t2 = val
+
+
+    return fib[n-1]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t1T2n = input().split()
+
+    t1 = int(t1T2n[0])
+
+    t2 = int(t1T2n[1])
+
+    n = int(t1T2n[2])
+
+    result = fibonacciModified(t1, t2, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
